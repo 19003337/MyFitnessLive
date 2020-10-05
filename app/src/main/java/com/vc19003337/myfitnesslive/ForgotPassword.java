3cb0c3fa3c2b0356377ddadf28295854f733c227
Charmaine Dobbin <19003337@vcconnect.co.za>
2020-10-05 10:24:22 +0200

    Initial commit

---
+++ app/src/main/java/com/vc19003337/myfitnesslive/ForgotPassword.java
@@ -0,0 +1,16 @@
+package com.vc19003337.myfitnesslive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPassword extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/vc19003337/myfitnesslive/MainActivity.java
@@ -0,0 +1,99 @@
+package com.vc19003337.myfitnesslive;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText email, password;
+    Button login, register, forgotPassword;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.et_password);
+        login = findViewById(R.id.btn_signIn);
+        register = findViewById(R.id.btn_signUp);
+        forgotPassword = findViewById(R.id.btn_forgotPassword);
+
+        register.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent openNewActivity = new Intent(MainActivity.this, ForgotPassword.class);
+                startActivity(openNewActivity);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String enteredEmail = email.getText().toString().trim();
+                String enteredPassword = password.getText().toString().trim();
+
+                mAuth.signInWithEmailAndPassword(enteredEmail, enteredPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, "Logged in "
+                                            + mAuth.getCurrentUser().getEmail() + " successfully",
+                                    Toast.LENGTH_SHORT).show();
+
+                            Intent openNewActivity = new Intent(MainActivity.this, ForgotPassword.class);
+                            startActivity(openNewActivity);
+                        }
+
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "Invalid credentials entered, no login for you!!!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener()
+                {
+                    @Override
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent openNewActivity = new Intent(MainActivity.this, ForgotPassword.class);
+                startActivity(openNewActivity);
+            }
+        });
+
+
+    }
+}
