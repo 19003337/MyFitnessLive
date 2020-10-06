079bfdaeb9469fa564be131d32ed7cb781731cdf
Charmaine Dobbin <19003337@vcconnect.co.za>
2020-10-06 15:56:25 +0200

    Pushing - WaterIntakeScreen.

---
+++ app/src/main/java/com/vc19003337/myfitnesslive/WaterIntakeScreen.java
@@ -0,0 +1,15 @@
+package com.vc19003337.myfitnesslive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaterIntakeScreen extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_intake_screen);
+    }
+}
