0bd24988b344f868f4846e6b21551f2ab12841eb
Charmaine Dobbin <19003337@vcconnect.co.za>
2020-10-23 12:02:56 +0200

    Pushing - Working on camera code.

--- app/src/main/java/com/vc19003337/myfitnesslive/AddMeal.java
+++ app/src/main/java/com/vc19003337/myfitnesslive/AddMeal.java
@@ -1,9 +1,16 @@
 package com.vc19003337.myfitnesslive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,6 +31,8 @@ import java.util.Calendar;
 
 public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
+    public static final int CAMERA_PERMISSION_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     private FirebaseAuth mAuth;
     FirebaseUser currentUser;
@@ -33,7 +42,7 @@ public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSele
     ImageView photoToUploadIV;
     TextView displayDateTodayTV;
     EditText mealDescriptionET, caloriesET, proteinET, fatET, carbohydratesET, cholesterolET, fiberET, sodiumET, potassiumET;
-    Button save;
+    Button saveBTN, cameraBTN, galleryBTN;
     Meals meals;
 
     @Override
@@ -61,14 +70,75 @@ public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSele
         fiberET = findViewById(R.id.et_fiber);
         sodiumET = findViewById(R.id.et_sodium);
         potassiumET = findViewById(R.id.et_potassium);
+        saveBTN = findViewById(R.id.btn_Save);
+        cameraBTN = findViewById(R.id.btn_Camera);
+        galleryBTN = findViewById(R.id.btn_Gallery);
 
         final Spinner spinnerMealTypes = findViewById(R.id.spinner_MealTypes);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(AddMeal.this, R.array.mealTypes, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMealTypes.setAdapter(adapter);
         spinnerMealTypes.setOnItemSelectedListener(this);
+
+        cameraBTN.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                AskCameraPermissions();
+            }
+        });
+
+        galleryBTN.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+
+        saveBTN.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
     }
 
+    private void AskCameraPermissions()
+    {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+        else {
+            OpenCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == CAMERA_PERMISSION_CODE)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                OpenCamera();
+            }
+            else {
+                Toast.makeText(this, "Camera Permission is required to use camera", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void OpenCamera()
+    {
+        Intent camera = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, CAMERA_REQUEST_CODE);
+    }
+    
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l)
     {
