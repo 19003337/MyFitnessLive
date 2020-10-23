b03d30bf96765fb55808760f6f8d088f9273c72d
Charmaine Dobbin <19003337@vcconnect.co.za>
2020-10-23 15:31:12 +0200

    Pushing - Added camera and gallery code.

--- app/src/main/java/com/vc19003337/myfitnesslive/AddMeal.java
+++ app/src/main/java/com/vc19003337/myfitnesslive/AddMeal.java
@@ -1,17 +1,27 @@
 package com.vc19003337.myfitnesslive;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -26,18 +36,22 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     public static final int CAMERA_PERMISSION_CODE = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 105;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     private FirebaseAuth mAuth;
     FirebaseUser currentUser;
     Calendar calendar;
-    String mealTypeSelected, dateToday, mealDescription;
+    String mealTypeSelected, dateToday, mealDescription, currentPhotoPath;
     Image photoToUpload;
     ImageView photoToUploadIV;
     TextView displayDateTodayTV;
@@ -94,7 +108,8 @@ public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSele
             @Override
             public void onClick(View view)
             {
-
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, GALLERY_REQUEST_CODE);
             }
         });
 
@@ -115,7 +130,7 @@ public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSele
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
         }
         else {
-            OpenCamera();
+            dispatchTakePictureIntent();
         }
     }
 
@@ -125,7 +140,7 @@ public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSele
         {
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
-                OpenCamera();
+                dispatchTakePictureIntent();
             }
             else {
                 Toast.makeText(this, "Camera Permission is required to use camera", Toast.LENGTH_SHORT).show();
@@ -133,11 +148,6 @@ public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSele
         }
     }
 
-    private void OpenCamera()
-    {
-        Intent camera = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camera, CAMERA_REQUEST_CODE);
-    }
     
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l)
@@ -156,4 +166,87 @@ public class AddMeal extends AppCompatActivity implements AdapterView.OnItemSele
     {
         //Nothing to code here at this stage
     }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                File f = new File(currentPhotoPath);
+                photoToUploadIV.setImageURI(Uri.fromFile(f));
+                Log.d("tag", "Absolute Url of Image is " + Uri.fromFile(f));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+            }
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+               Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
+                Log.d("tag", "onActivityRequest: Gallery Image Uri: " + imageFileName);
+                photoToUploadIV.setImageURI(contentUri);
+            }
+        }
+    }
+
+    private String getFileExt(Uri contentUri)
+    {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
+
+    private File createImageFile() throws IOException
+    {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent()
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+        {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.vc19003337.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
 }
